f03cffd8f35049d8cd169a06ef2f611362f44545
Nanda Gopal <nandagopal@example.com>
2025-11-24 14:17:39 +0530

    feat: add complete react-TaskManager project

--- backend/routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {
-  getProducts,
-  getSingleProduct,
-} = require("../controllers/productControllers");
-const router = express.Router();
-router.route("/products").get(getProducts);
-router.route("/products/:id").get(getSingleProduct);
-module.exports = router;
